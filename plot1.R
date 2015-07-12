4ff924ea4949b5cc110b6325eb574a4796f5283f
JuanLopez96 <juanl.jumanji@hotmail.es>
2015-07-12 18:51:56 +0200

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,3 @@
+png(file="plot1.png",width=480,height=480)
+hist(household_power_consumption$Global_active_power, main="Global Active Power", xlab= "Global Active Power (kilowatts)", col="red", bg="transparent")
+dev.off()
