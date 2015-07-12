81e6e05ff5fe93537f8b5762e8eee8df10768bd5
JuanLopez96 <juanl.jumanji@hotmail.es>
2015-07-12 20:34:59 +0200

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -4,3 +4,5 @@ endDay = as.POSIXct(strftime("2007-02-03 00:00:00"))
 plot(household_power_consumption$Time, household_power_consumption$Global_active_power, xlim=c(startDay, endDay), xaxt="n", xlab="", ylab="Global Active Power (kilowatts)", type="l")
 axis.POSIXct(1, at=seq(startDay, endDay, by="day"), format="%a")
 dev.off()
+
+## Please note that the abbreviations of the days of the week appear in spanish (ju. = jueves; vi. = viernes; sá. = sábado). 
