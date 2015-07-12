6f1b0564394a547a9723de9d1791111d730d7da0
JuanLopez96 <juanl.jumanji@hotmail.es>
2015-07-12 18:53:07 +0200

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,6 @@
+png(file='plot2.png', width=480, height=480, units='px')
+startDay = as.POSIXct(strftime("2007-02-01 00:00:00"))
+endDay = as.POSIXct(strftime("2007-02-03 00:00:00"))
+plot(household_power_consumption$Time, household_power_consumption$Global_active_power, xlim=c(startDay, endDay), xaxt="n", xlab="", ylab="Global Active Power (kilowatts)", type="l")
+axis.POSIXct(1, at=seq(startDay, endDay, by="day"), format="%a")
+dev.off()
